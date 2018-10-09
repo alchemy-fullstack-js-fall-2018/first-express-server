b3ec56967150dd31caa4030320c5c15938861294
Jack <ajackofsometrades@gmail.com>
2018-10-08 18:34:14 -0700

    post method is working

--- test/app.test.js
+++ test/app.test.js
@@ -1,46 +1,21 @@
 require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
+const db = require('../lib/mongo-connection');
 const Aircraft = require('../lib/models/Aircraft');
 
 
 describe('Aircraft E2E test', () => {
 
-    const aircrafts = [
-        { type: 'AH-64', nickname: 'Apache', speed: 227, released: 1986, active: true },
-        { type: 'F-16', nickname: 'Fighting Falcon', speed: 1500, released: 1978, active: true },
-        { type: 'SR-71', nickname: 'Blackbird', speed: 2200, released: 1966, active: false }
-    ];
-
-    let createdAircrafts;
-
-    const lockheedMartin = aircraft => {
+    it('creates an aircraft on post', () => {
         return request(app)
-            .post('/aircrafts')
-            .send(aircraft);
-    };
-
-    beforeEach(() => {
-        Aircraft.drop();
+            .post('/api/aircrafts')
+            .send({ type: 'A-10', nickname: 'Thunderbird', speed: 439, released: 1977, active: true })
+            .then(res => expect(res.body).toEqual({ _id: expect.any(String), type: 'A-10', nickname: 'Thunderbird', speed: 439, released: 1977, active: true }));
     });
-    beforeEach(() => {
-        return Promise.all(aircrafts.map(lockheedMartin))
-            .then(ca => createdAircrafts = ca.map(a => a.body));
-    });
-
-    // alt:
-    // beforeEach(async() => {
-    //     const ca = await Promise.all(aircrafts.map(lockheedMartin));
-    //     return createdAircrafts = ca.map(a => a.body);
-    // });
-
-    it('get an aircraft by id', () => {
-        return request(app)
-            .get(`/aircrafts/${createdAircrafts[0]._id}`)
-            .then(res => expect(res.body).toEqual(createdAircrafts[0]));
-    });
-
 
 
 
 });
+
+
