375f821944b820bbffa58c2b97e4c7318201e994
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-08 17:37:54 -0700

    update and remove working in models

--- test/celebs.test.js
+++ test/celebs.test.js
@@ -32,12 +32,26 @@ describe('Celebs', () => {
             });
     });
 
-    it('creates a celeb in my db', () => {
+    it('creates a Celeb in my db', () => {
         return Celebs.create('Jack Sparrow', 'Pirate')
             .then(createdCeleb => {
                 expect(createdCeleb).toHaveProperty('_id');
                 expect(createdCeleb.name).toEqual('Jack Sparrow');
                 expect(createdCeleb.job).toEqual('Pirate');
+            });
+    });
+
+    it('updates a Celeb by id', () => {
+        return Celebs.update(createdCelebs[0]._id, { name: 'Sterling Archer' })
+            .then(receivedCeleb => {
+                expect(receivedCeleb).toEqual({ ...createdCelebs[0], name: 'Sterling Archer' });
+            });
+    });
+
+    it('deletes a Celeb by id', () => {
+        return Celebs.delete(createdCelebs[0]._id)
+            .then(result => {
+                expect(result.removed).toEqual(true);
             });
     });
 });
