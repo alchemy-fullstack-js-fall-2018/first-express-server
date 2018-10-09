9c4de154a78f736b5c92a00054533ffb3eec6553
Jack <ajackofsometrades@gmail.com>
2018-10-08 18:36:21 -0700

    get by id is working

--- test/app.test.js
+++ test/app.test.js
@@ -7,6 +7,46 @@ const Aircraft = require('../lib/models/Aircraft');
 
 describe('Aircraft E2E test', () => {
 
+
+    const aircrafts = [
+        { type: 'AH-64', nickname: 'Apache', speed: 227, released: 1986, active: true },
+        { type: 'F-16', nickname: 'Fighting Falcon', speed: 1500, released: 1978, active: true },
+        { type: 'SR-71', nickname: 'Blackbird', speed: 2200, released: 1966, active: false }
+    ];
+
+    let createdAircrafts;
+
+    const lockheedMartin = aircraft => {
+        return request(app)
+            .post('/api/aircrafts')
+            .send(aircraft);
+    };
+
+    beforeEach(() => {
+        Aircraft.drop();
+    });
+    beforeEach(() => {
+        return Promise.all(aircrafts.map(lockheedMartin))
+            .then(ca => createdAircrafts = ca.map(a => a.body));
+    });
+
+    // alt:
+    // beforeEach(async() => {
+    //     const ca = await Promise.all(aircrafts.map(lockheedMartin));
+    //     return createdAircrafts = ca.map(a => a.body);
+    // });
+
+    it('get an aircraft by id', () => {
+        return request(app)
+            .get(`/api/aircrafts/${createdAircrafts[0]._id}`)
+            .then(res => {
+                console.log(res.body);
+                console.log(createdAircrafts[0]);
+                expect(res.body).toEqual(createdAircrafts[0]);
+            });
+    });
+
+
     it('creates an aircraft on post', () => {
         return request(app)
             .post('/api/aircrafts')
