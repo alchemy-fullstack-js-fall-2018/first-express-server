a74ef212ce957d8955f75c7dc730f548d32018a1
Jack <ajackofsometrades@gmail.com>
2018-10-08 18:56:23 -0700

    update is working

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const request = require('supertest');
 const app = require('../lib/app');
 const db = require('../lib/mongo-connection');
-const Aircraft = require('../lib/models/Aircraft');
 
 
 describe('Aircraft E2E test', () => {
@@ -23,7 +22,7 @@ describe('Aircraft E2E test', () => {
     };
 
     beforeEach(() => {
-        Aircraft.drop();
+        return db('aircraft').then(collection => collection.deleteMany());
     });
     beforeEach(() => {
         return Promise.all(aircrafts.map(lockheedMartin))
@@ -57,6 +56,17 @@ describe('Aircraft E2E test', () => {
             .post('/api/aircrafts')
             .send({ type: 'A-10', nickname: 'Thunderbird', speed: 439, released: 1977, active: true })
             .then(res => expect(res.body).toEqual({ _id: expect.any(String), type: 'A-10', nickname: 'Thunderbird', speed: 439, released: 1977, active: true }));
+    });
+
+    it('updates an aircraft on put', () => {
+
+        const upgrade = { type: 'AH-65', nickname: 'Mohawk', speed: 999, released: 2020, active: true };
+        const expected = { ...upgrade, _id: expect.any(String) };
+        return request(app)
+            .put(`/api/aircrafts/${createdAircrafts[0]._id}`)
+            .send(upgrade)
+            .then(res => expect(res.body).toEqual(expected));
+
     });
 
     it('demolishes an aircraft on delete', () => {
