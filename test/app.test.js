96cfcff860af607e513e4d2cf6d0bf2c26d6501f
KLPainter <KLPainter@gmail.com>
2018-10-08 17:36:44 -0700

    3 tests passing

---
+++ test/app.test.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app');
+const db = require('../lib/mongo-connector');
+
+describe('event pub/sub API', () => {
+
+    let seedGames = [
+        { title: 'Blokus', numPlayers: 4, minutesPerGame: 30 },
+        { title: 'Settlers of Catan', numPlayers: 4, minutesPerGame: 60 },
+        { title: 'Chess', numPlayers: 2, minutesPerGame: 90 }
+    ];
+
+    let createdGames;
+
+    const makeGame = game => {
+        return request(app)
+            .post('/games')
+            .send(game)
+            .then(res => res.body);
+    };
+
+    beforeEach(() => {
+        return db('games').then(collection => collection.deleteMany());
+    });
+
+    beforeEach(() => {
+        return Promise.all(seedGames.map(makeGame)).then(gamesRes => {
+            createdGames = gamesRes;
+        });
+    });
+
+    it('creates a game on post', () => {
+        const newGame =  { title: 'Haunting', numPlayers: 6, minutesPerGame: 60 };
+        return request(app)
+            .post('/games')
+            .send(newGame)
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    title: newGame.title,
+                    numPlayers: newGame.numPlayers,
+                    minutesPerGame: newGame.minutesPerGame
+                });
+            });
+    });
+
+    it('gets all games on get', () => {
+        return request(app)
+            .get('/games')
+            .then(retrievedGames => {
+                createdGames.forEach(createdGame => {
+                    expect(retrievedGames.body).toContainEqual(createdGame);
+                });
+            });
+    });
+
+    it('gets a game by id', () => {
+        return request(app)
+            .get(`/games/${createdGames[0]._id}`)
+            .then(res => {
+                expect(res.body).toEqual(createdGames[0]);
+            });
+    });
+
+    it('deletes a game by id', () => {
+        return request(app)
+            .delete(`/games/${createdGames[0]._id}`)
+            .get(`/games/${createdGames[0]._id}`)
+            .then(res => {
+                expect(res.body).toEqual(null);
+            });
+    });
+});
