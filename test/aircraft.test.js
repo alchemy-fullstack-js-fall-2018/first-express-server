e821941da1df7ba189985f948459792c62c481d4
Jack <ajackofsometrades@gmail.com>
2018-10-08 17:22:13 -0700

    test written for getall

--- test/aircraft.test.js
+++ test/aircraft.test.js
@@ -3,13 +3,34 @@ const Aircraft = require('../lib/models/Aircraft');
 
 describe('aircraft model', () => {
 
+    let createdAircraft;
+
+    beforeEach(() => {
+        return Aircraft.drop();
+    });
+
+    beforeEach(() => {
+        return Promise.all([
+            Aircraft.create({ type: 'AH-64', nickname: 'Apache', speed: 227, released: 1986, active: true }),
+            Aircraft.create({ type: 'F-16', nickname: 'Fighting Falcon', speed: 1500, released: 1978, active: true }),
+            Aircraft.create({ type: 'SR-71', nickname: 'Blackbird', speed: 2200, released: 1966, active: false })
+        ])
+            .then(ca => createdAircraft = ca);
+    });
+
+    it('gets all aircraft', () => {
+        return Aircraft.getAll()
+            .then(receivedAircrafts => expect(receivedAircrafts).toHaveLength(3));
+    });
+
     it('creates a new aircraft in my db', () => {
-        return Aircraft.create({ type: 'A-10', speed: 439, released: 1994, active: true })
+        return Aircraft.create({ type: 'A-10', nickname: 'Thunderbird', speed: 439, released: 1977, active: true })
             .then(createdAircraft => {
                 expect(createdAircraft).toHaveProperty('_id');
                 expect(createdAircraft.type).toEqual('A-10');
+                expect(createdAircraft.nickname).toEqual('Thunderbird');
                 expect(createdAircraft.speed).toEqual(439);
-                expect(createdAircraft.released).toEqual(1994);
+                expect(createdAircraft.released).toEqual(1977);
                 expect(createdAircraft.active).toEqual(true);
             });
     });
