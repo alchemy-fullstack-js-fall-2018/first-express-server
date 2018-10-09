53556768446234b15dd40de5795e638e7895a532
Davidchhing <davidhchhing@gmail.com>
2018-10-09 09:32:14 -0700

    wrote create car test, get car test, and get by id

---
+++ test/app.test.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app');
+const db = require('../lib/mongo-connector');
+const Chance = require('chance');
+const chance = new Chance();
+
+describe('all about cars', () => {
+
+    let carsArray = Array.apply(null, { length: 100 }).map(() => {
+        return {
+            type: 'new',
+            make: 'honda',
+            year: chance.guid({ version: 4 }),
+            models: chance.guid({ version: 4 }),
+        };
+    });
+
+    let createdCars;
+
+    const createCar = cars => {
+        return request(app)
+            .post('api/cars')
+            .send(cars)
+            .then(res => res.body);        
+    };
+
+    beforeEach(() => {
+        return db('cars').then(collection => collection.deleteMany());
+    });
+
+    beforeEach(() => {
+        return Promise.all(carsArray.map(createCar)).then(carsResonse => {
+            createdCars = carsResonse;
+        });
+    });
+
+    it('creates new car info on post', () => {
+        return request(app)
+            .post('/api/cars')
+            .send({
+                type: 'new',
+                make: 'honda',
+                year: '2018',
+                models: 'accord'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    type: 'new',
+                    make: 'honda',
+                    year: '2018',
+                    models: 'accord'
+                });
+            });
+    });
+
+    it('gets all cars on get', () => {
+        return request(app)
+            .get('/api/cars')
+            .then(retrievedCars => {
+                createdCars.forEach(createdCarsInformation => {
+                    expect(retrievedCars.body).toContainEqual(createdCarsInformation);
+                });
+            });
+    });
+
+    it('gets a car by id', () => {
+        return request(app)
+            .get(`/api/cars/${createdCars[0]._id}`)
+            .then(res => {
+                expect(res.body).toEqual(createdCars[0]);
+            });
+    });
+
+
+
+
+   
+
+    // it('creates new car info on post', () => {
+
+    // });
+
+    // it('creates new car info on post', () => {
+
+    // });
+
+
+
+
+});
